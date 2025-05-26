0893898d1734ca2807a68d64ad31fb6dd75f858a
Gerardo Alberto Ramirez Mejorada <147885471+Pusstes@users.noreply.github.com>
2025-05-26 00:13:08 -0600

    practiva 1

---
+++ Practica1/ejercicio_A.js
@@ -0,0 +1,7 @@
+let nombre = "Armando";
+const edad = 25;
+
+nombre ="Ana María";
+
+const saludo = "Hola, mi nombre es " + nombre + ". Tienes " + edad + " años.";
+console.log(saludo);
---
+++ Practica1/ejercicio_C.js
@@ -0,0 +1,4 @@
+const saludoPersonalixado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años`;
+
+const saludo = saludoPersonalixado("Isay", 37);
+console.log(saludo);
