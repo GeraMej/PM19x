d11e405feb712518fef58fc33db78d772495dcaf
Gerardo Alberto Ramirez Mejorada <147885471+Pusstes@users.noreply.github.com>
2025-06-24 15:03:58 -0600

    Parte de emilio Marquez

--- PracticaBotonSwitch/index.js
+++ PracticaBotonSwitch/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './TextInput';
+import App from './MachBackground';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
